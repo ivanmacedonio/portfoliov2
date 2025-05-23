84128e44707e1aa176219bed7016a22e0876ad21
Ivan Macedonio <ivanmacedonio778@gmail.com>
2025-05-23 16:29:45 -0300

    a

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -46,7 +46,7 @@ const projectsList = [
     title: "Proceso de facturación concurrente",
     description:
       "Proceso de facturación desarrollado con Flask aplicando programación concurrente, arquitectura hexagonal y third-party AFIPWS",
-    url: "https://github.com/ivanmacedonio",
+    url: "https://github.com",
     image: web7,
   },
   {
